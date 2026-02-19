6d64e52f226f2ea447eff4dcbb7d293756cb6f1a
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-02-19 19:25:26 +0400

    Refactor main function for shell implementation

--- main.c
+++ main.c
@@ -1,19 +1,65 @@
-if (args[0])
+#include "shell.h"
+
+int last_status = 0;
+
+/**
+ * main - simple shell
+ * Return: exit status
+ */
+int main(void)
 {
-    if (strcmp(args[0], "exit") == 0)
-        break;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char *token;
+	char *args[100];
+	int i;
 
-    if (strcmp(args[0], "env") == 0)
-    {
-        int j = 0;
-        while (environ[j])
-        {
-            write(STDOUT_FILENO, environ[j], strlen(environ[j]));
-            write(STDOUT_FILENO, "\n", 1);
-            j++;
-        }
-        continue;
-    }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
 
-    execute_command(args);
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+			break;
+
+		if (read > 1 && line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		i = 0;
+		token = strtok(line, " \t");
+		while (token && i < 99)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " \t");
+		}
+		args[i] = NULL;
+
+		if (!args[0])
+			continue;
+
+		/* EXIT built-in */
+		if (strcmp(args[0], "exit") == 0)
+			break;
+
+		/* ENV built-in */
+		if (strcmp(args[0], "env") == 0)
+		{
+			int j = 0;
+			while (environ[j])
+			{
+				write(STDOUT_FILENO, environ[j], strlen(environ[j]));
+				write(STDOUT_FILENO, "\n", 1);
+				j++;
+			}
+			continue;
+		}
+
+		/* Other commands */
+		execute_command(args);
+	}
+
+	free(line);
+	return (last_status);
 }
