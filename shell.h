a3f81ed682a55b254e42c4d2404129b181601572
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-02-19 18:47:43 +0400

    Update shell.h with additional headers and functions
    Added necessary headers and function declarations for command execution.

--- shell.h
+++ shell.h
@@ -1,7 +1,12 @@
-#ifndef _SHELL_H
-#define _SHELL_H
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
-#endif
+extern char **environ;
+
+char *find_command(char *command);
+void execute_command(char **args);
