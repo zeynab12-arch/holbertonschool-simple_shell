20ea72a1796515b27b5ea130578f5ba6307f62bf
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-02-19 18:54:13 +0400

    Remove sys/stat.h include from shell.h

--- shell.h
+++ shell.h
@@ -1,12 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
 
 extern char **environ;
 
 char *find_command(char *command);
 void execute_command(char **args);
+
+#endif
