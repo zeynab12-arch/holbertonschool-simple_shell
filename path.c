c571e64ca1707a7cf0242086079e1ad683598890
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-02-22 15:54:12 +0400

    Add find_command function to search for commands
    Implement find_command function to locate executable commands in PATH.

--- path.c
+++ path.c
@@ -1,1 +1,44 @@
+#include "shell.h"
 
+char *find_command(char *command)
+{
+	char *path, *path_copy, *dir, *full_path;
+	int len;
+
+	if (access(command, X_OK) == 0)
+		return (strdup(command));
+
+	path = getenv("PATH");
+	if (!path)
+		return (NULL);
+
+	path_copy = strdup(path);
+	if (!path_copy)
+		return (NULL);
+
+	dir = strtok(path_copy, ":");
+
+	while (dir != NULL)
+	{
+		len = strlen(dir) + strlen(command) + 2;
+		full_path = malloc(len);
+		if (!full_path)
+			return (NULL);
+
+		strcpy(full_path, dir);
+		strcat(full_path, "/");
+		strcat(full_path, command);
+
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (NULL);
+}
